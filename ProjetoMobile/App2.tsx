c6dd61b06fbf8b376e077f4b9a0dff217f3b8860
Guilherme <130716043+GuilhermeTDA@users.noreply.github.com>
2023-10-06 20:30:52 -0300

    aula06/10

--- ProjetoMobile/App2.tsx
+++ ProjetoMobile/App2.tsx
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     StyleSheet,
-  Text, View
+    Text,
+    View,
+    TextInput,
+    Image,
+    ScrollView,
 } from 'react-native';
 
+const logo = {
+    uri: 'https://reactnative.dev/img/tiny_logo.png',
+    width: 64,
+    height: 64,
+  };
 const App2 = () => {
+    const [minhaString,setText ] = useState('');
     return (
-        <>   
-         <Text> Hello word</Text>
-            <Text> Hello word</Text>
-            <Text> Hello word</Text>
-            <Text> Hello word</Text>
-            <Text> Hello word</Text>
-            <Text> Hello word</Text>
-            <Text> Hello word</Text>
-            <Text> Hello word</Text>
-            <View style={styles.container}><Text>pampas</Text></View>
-        </>
+        <>
+             <View>
+      <Text>{minhaString}</Text>
+      <TextInput
+        placeholder="Digite algo aqui..."
+        style={{ borderWidth: 1, borderColor: 'gray', padding: 10}}
+         onChangeText={newText => setText(newText)}
+    
+    defaultValue={minhaString}
+      />
+      
+    </View>
+    <View>
+    <Text style={{padding: 10, fontSize: 42}}>
+        {minhaString }
+      </Text>
+    </View>
+
+    <ScrollView>
+    <Text style={{fontSize: 96}}>Scroll me plz</Text>
+    <Image source={bolso.png} />
+    <Image source={bolso.png} />
+    <Image source={bolso.png} />
+    <Image source={bolso.png} />
+    </ScrollView>
+    </>
     );
 }
 
